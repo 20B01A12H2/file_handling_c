bfc206ba51765e060fd1ae303ab7308f51c79443
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-26 17:57:05 +0530

    ternary operator code in datatypes_practise.c

--- c_practise/datatypes_practise.c
+++ c_practise/datatypes_practise.c
@@ -1,48 +1,48 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-//     int len = 20;
-//     int breadth = 10;
-//     int area = len * breadth;
+int main(){
+    int len = 20;
+    int breadth = 10;
+    int area = len * breadth;
 
-//     printf("%d", area);
-//     return 0;
-// }
-
-
-// /* ASCII values A - 65, a - 97*/
-// #include <stdio.h>
-
-// int main(){
-//     int a = 65;
-//     int b = 66;
-//     int c = 67;
-
-//     printf("A - %c\nB - %c\nC - %c", a, b, c);
-//     return 0;
-// }
+    printf("%d", area);
+    return 0;
+}
 
 
-// #include <stdio.h>
+/* ASCII values A - 65, a - 97*/
+#include <stdio.h>
 
-// int main(){
-//     char mychar = 'Hello';
+int main(){
+    int a = 65;
+    int b = 66;
+    int c = 67;
+
+    printf("A - %c\nB - %c\nC - %c", a, b, c);
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main(){
+    char mychar = 'Hello';
     
-//     printf("%c", mychar);   // only stores last character
-//     printf("%s", mychar);   // stores all characters
-//     return 0;
-// }
+    printf("%c", mychar);   // only stores last character
+    printf("%s", mychar);   // stores all characters
+    return 0;
+}
 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-//     printf("%lu\n", sizeof(int));
-//     printf("%lu\n", sizeof(float));
-//     printf("%lu\n", sizeof(double));
-//     printf("%lu\n", sizeof(char));
-//     return 0;
-// }
+int main(){
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(float));
+    printf("%lu\n", sizeof(double));
+    printf("%lu\n", sizeof(char));
+    return 0;
+}
 
 
 #include <stdio.h>
@@ -56,5 +56,16 @@ int main(){
     }else{
         printf("%d years (< 18) old people are not allowed to vote", myAge);
     }
+    return 0;
+}
+
+/* using ternary operator 
+variable = (condition) ? expressionTrue : expressionFalse; */
+
+#include <stdio.h>
+
+int main(){
+    int time = 18;
+    (time < 20) ? printf("Good day.") : printf("Good Evening.");
     return 0;
 }
