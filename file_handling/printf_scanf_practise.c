20f93374a106bad2ceacd90087ef5c8af5f1e49a
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-26 10:12:27 +0530

    sn1 of printf, scanf practise file

---
+++ file_handling/printf_scanf_practise.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int mynum = 9;
+    float myfloat = 19.9;
+    char mychar = 'A';
+
+    printf("my number is %d my float number is %f my character is %c", mynum, myfloat, mychar);
+    return 0;
+}
