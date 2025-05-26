06bc037ac7a28f5519c2232e3e0260edba849e28
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-26 17:43:15 +0530

    sizeof datatypes and voting code in datatypes_practise.c

--- c_practise/datatypes_practise.c
+++ c_practise/datatypes_practise.c
@@ -1,34 +1,60 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
-    int len = 20;
-    int breadth = 10;
-    int area = len * breadth;
+// int main(){
+//     int len = 20;
+//     int breadth = 10;
+//     int area = len * breadth;
 
-    printf("%d", area);
-    return 0;
-}
-
-
-/* ASCII values A - 65, a - 97*/
-#include <stdio.h>
-
-int main(){
-    int a = 65;
-    int b = 66;
-    int c = 67;
-
-    printf("A - %c\nB - %c\nC - %c", a, b, c);
-    return 0;
-}
+//     printf("%d", area);
+//     return 0;
+// }
 
 
-#include <stdio.h>
+// /* ASCII values A - 65, a - 97*/
+// #include <stdio.h>
 
-int main(){
-    char mychar = 'Hello';
+// int main(){
+//     int a = 65;
+//     int b = 66;
+//     int c = 67;
+
+//     printf("A - %c\nB - %c\nC - %c", a, b, c);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     char mychar = 'Hello';
     
-    printf("%c", mychar);   // only stores last character
-    printf("%s", mychar);   // stores all characters
+//     printf("%c", mychar);   // only stores last character
+//     printf("%s", mychar);   // stores all characters
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("%lu\n", sizeof(int));
+//     printf("%lu\n", sizeof(float));
+//     printf("%lu\n", sizeof(double));
+//     printf("%lu\n", sizeof(char));
+//     return 0;
+// }
+
+
+#include <stdio.h>
+
+int main(){
+    int myAge = 25;
+    int votingAge = 18;
+
+    if (myAge >= votingAge){
+        printf("%d years (> 18) old people are allowed to vote", myAge);
+    }else{
+        printf("%d years (< 18) old people are not allowed to vote", myAge);
+    }
     return 0;
 }
