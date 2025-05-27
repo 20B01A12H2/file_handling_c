d257ba30649e2298fad7d206acae2df34d7817d2
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-27 10:29:24 +0530

    countdown to happy new year in while_loop.c

--- c_practise_27_5_25/while_loop.c
+++ c_practise_27_5_25/while_loop.c
@@ -1,11 +1,26 @@
-/* while loop - printing 10 natural numbers */
+// /* while loop - printing 10 natural numbers */
+// #include <stdio.h>
+
+// int main(){
+//     int i = 1;
+//     while (i <= 10){
+//         printf("%d\n", i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+/* countdown to happy new year */
+
 #include <stdio.h>
 
 int main(){
-    int i = 1;
-    while (i <= 10){
-        printf("%d\n", i);
-        i++;
+    int countdown = 3;
+
+    while (countdown > 0){
+        printf("%d\n", countdown);
+        countdown--;
     }
-    return 0;
+    printf("Happy New Year!");
 }
