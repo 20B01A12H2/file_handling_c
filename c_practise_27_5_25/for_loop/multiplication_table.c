0cb0af57b0ad1a3deb18cb5487d2fc0baa416fea
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-27 17:22:07 +0530

    multiplication table

---
+++ c_practise_27_5_25/for_loop/multiplication_table.c
@@ -0,0 +1,11 @@
+/* multiplication table for a number */
+
+#include <stdio.h>
+
+int main(){
+    int num = 5;
+    for (int i=1; i <= 10; i++){
+        printf("%d * %d = %d\n", num, i, num*i);
+    }
+    return 0;
+}
