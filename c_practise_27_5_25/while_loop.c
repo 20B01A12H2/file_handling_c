27cce4f9fac7ac19d0c7102301ba945b3b6d7163
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-27 10:17:03 +0530

    while_loop.c in c_practise_2_5_25

---
+++ c_practise_27_5_25/while_loop.c
@@ -0,0 +1,11 @@
+/* while loop - printing 10 natural numbers */
+#include <stdio.h>
+
+int main(){
+    int i = 1;
+    while (i <= 10){
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
