5cc55c102f568a3ca4620392a536a71188065d7d
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-27 16:36:44 +0530

    separated files to different folder like while_loop, for_loop

---
+++ c_practise_27_5_25/for_loop/natural_nums_for.c
@@ -0,0 +1,11 @@
+/* print natural numbers using for loop */
+
+#include <stdio.h>
+
+int main(){
+    printf("10 natural numbers are: ");
+    for (int i = 0; i <= 10; i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}
