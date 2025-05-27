a0dfbb2effe7e43982ea160d850384571fd132ed
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-05-27 16:06:54 +0530

    even numbers using while loop

---
+++ c_practise_27_5_25/even_numbers_whileloop.c
@@ -0,0 +1,12 @@
+/* print even numbers using while loop */
+
+#include <stdio.h>
+
+int main(){
+    int i = 0;
+    while (i <= 10){
+        printf("%d\n", i);
+        i += 2;
+    }
+    return 0;
+}
