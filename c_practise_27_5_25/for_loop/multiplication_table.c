ce7cb6ee8bc916d3ffbc41d6fec9eebcb5a20923
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-01 18:09:57 +0530

    added brackets

--- c_practise_27_5_25/for_loop/multiplication_table.c
+++ c_practise_27_5_25/for_loop/multiplication_table.c
@@ -5,7 +5,7 @@
 int main(){
     int num = 5;
     for (int i = 1; i <= 10; i++){
-        printf("%d * %d = %d\n", num, i, num*i);
+        printf("%d * %d = %d\n", num, i, (num*i));
     }
     return 0;
 }
