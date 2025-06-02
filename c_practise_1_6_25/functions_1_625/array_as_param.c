e335cab74bc864c7343f2f126b480bd28e0cf66a
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-02 13:00:12 +0530

    changes to array_as_param.c

--- c_practise_1_6_25/functions_1_625/array_as_param.c
+++ c_practise_1_6_25/functions_1_625/array_as_param.c
@@ -1,21 +1,3 @@
-// #include <stdio.h>
-
-// void array_param(int myArray[], int size){  // int* myArray
-//     printf("address of pointer myArray - %zu\n", sizeof(myArray));
-//     printf("%zu\n", &myArray);
-//     printf("value in myArray - %zu\n", myArray);
-// }
-
-// int main(){
-//     int myNums[] = {10, 20, 30, 40, 50};
-//     array_param(myNums, 5);
-//     printf("address of myNums Array - %zu\n", &myNums);
-//     printf("number of bytes allocated - %d\n", sizeof(myNums));
-//     printf("value - %d\n", myNums[0]);
-
-//     return 0;
-// }
-
 #include <stdio.h>
 
 void array_param(int myArray[], int size){  // int* myArray
