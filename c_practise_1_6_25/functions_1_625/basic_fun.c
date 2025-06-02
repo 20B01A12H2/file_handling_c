fbcee8e007ca8b55453a034e89edb45a3ba4a382
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-02 09:48:54 +0530

    basic function

---
+++ c_practise_1_6_25/functions_1_625/basic_fun.c
@@ -0,0 +1,35 @@
+/*
+.\basic_fun.c:6:12: warning: 'return' with a value, in function returning void
+    6 |     return 0;
+      |            ^
+.\basic_fun.c:3:6: note: declared here
+    3 | void my_fun(){
+      |      ^~~~~~
+*/
+// #include <stdio.h>
+
+// void my_fun(){
+//     printf("Hello world");
+
+//     return 0;
+// }
+
+// int main(){
+//     my_fun();
+
+//     return 0;
+// }
+
+
+/* Hello world */
+#include <stdio.h>
+
+void my_fun(){
+    printf("Hello world");
+}
+
+int main(){
+    my_fun();
+
+    return 0;
+}
