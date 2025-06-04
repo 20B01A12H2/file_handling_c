3f3bc480bc25e7c2fa5b3b68cda584e0773b8967
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-04 17:57:44 +0530

    added fclose() function in read_data.c file

--- c_practise_4_6_25/file_handling/read_data.c
+++ c_practise_4_6_25/file_handling/read_data.c
@@ -2,11 +2,13 @@
 
 int main(){
     FILE *fptr;
-    fptr = fopen("sample.txt", "r");
+    fptr = fopen("c.txt", "r");
     
     char myString[100];
     fgets(myString, 200, fptr);
     printf("%s", myString);
+
+    fclose(fptr);
 
     return 0;
 }
