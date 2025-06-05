c5970180408d76154007e67e04dffa333cda72be
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-05 16:18:18 +0530

    declaring values in a single line

---
+++ c_practise_5_6_25/struct_decl_1line.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct mystruct {
+    char name[20];
+    int age;
+    char letter;
+};
+
+int main(){
+    struct mystruct s1 = {"rama", 25, 'A'};
+    struct mystruct s2 = {"sita", 28, 'B'};
+
+    printf("names are : %s, %s\n", s1.name, s2.name);
+    printf("ages are : %d, %d\n", s1.age, s2.age);
+    printf("letters are : %c, %c\n", s1.letter, s2.letter);
+
+    return 0;
+}
