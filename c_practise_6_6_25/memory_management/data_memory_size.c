43598fcfbd0003c9296e60b4353f7b0b3cdb3189
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-06 16:34:49 +0530

    finding memory space allocated to datatypes

---
+++ c_practise_6_6_25/memory_management/data_memory_size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    float float_num;
+    char letter;
+    char name[26];
+
+    printf("size of int - %d\n", sizeof(int));
+    printf("size of float - %d\n", sizeof(float));
+    printf("size of char - %d\n", sizeof(letter));
+    printf("size of char_string - %d\n", sizeof(name));
+
+    return 0;
+}
