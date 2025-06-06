0efe6302d59539582bf7b427774070de1559eb21
20B01A12H2 <20B01A12H2@svecw.edu.in>
2025-06-06 17:16:42 +0530

    multiplication table of user given number

---
+++ c_practise_6_6_25/c_programs/multiplication_table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("enter number - ");
+    scanf("%d", &num);
+
+    int i = 1;
+    while (i <= 10){
+        printf("%d * %d = %d\n", num, i, num*i);
+        i++;
+    }
+
+    return 0;
+}
